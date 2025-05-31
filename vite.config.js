bfcc57134bebd4444825da0e2e756d3767e38b02
Artem <tem4ik20.1@gmail.com>
2025-05-31 16:20:50 +0300

    complete task

--- vite.config.js
+++ vite.config.js
@@ -2,47 +2,37 @@ import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
+import postcssSortMediaQueries from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+      'global': {}, 
     },
-    root: 'src',
+    root: 'src', 
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/*.html'), 
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
+            if (id.includes('node_modules')) return 'vendor';
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
       },
       outDir: '../dist',
       emptyOutDir: true,
     },
+    css: {
+      postcss: {
+        plugins: [postcssSortMediaQueries({ sort: 'mobile-first' })],
+      },
+    },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      FullReload(['./src/**/*.html']),
     ],
   };
 });
